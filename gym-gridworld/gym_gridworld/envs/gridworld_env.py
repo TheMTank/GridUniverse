e5f2873d57195d260d12e9e674911b3bc5c62053
Unknown <fgtoralesch@gmail.com>
2017-11-18 02:51:23 +0100

    Add functionalities for extension
    World is separated as a class varialbe and it has a generator and an update function. 
    Additionally now there is another function to check if the steps are valid (outside the grid, but in the future could be against a wall).
    Renamed x_y_dim for clarity.

--- gym-gridworld/gym_gridworld/envs/gridworld_env.py
+++ gym-gridworld/gym_gridworld/envs/gridworld_env.py
@@ -10,36 +10,61 @@ from six import StringIO
 class GridWorldEnv(gym.Env):
     metadata = {'render.modes': ['human', 'ansi', 'graphic']}
 
-    def __init__(self, x_y_dim=4):
+    def __init__(self, grid_dim=(4, 4)):
+        self.x_max = grid_dim[0]
+        self.y_max = grid_dim[1]
         self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Box(spaces.Discrete(x_y_dim), spaces.Discrete(x_y_dim))
+        self.observation_space = spaces.Box(spaces.Discrete(self.x_max), spaces.Discrete(self.y_max))
         # self.reward_range = None  # by default [-inf, inf]
 
-        self.x_y_dim = x_y_dim
         self.done = False
+        self.prev_state = [0, 0]
         self.curr_state = [0, 0]
-        self.terminal_state = (self.x_y_dim-1, self.x_y_dim-1)
+        self.terminal_state = (self.x_max-1, self.y_max-1)
         self.info = {}
+        self.world = self._generate_world()
+
+    def _is_valid(self, state):
+        """
+        Checks if a given state is inside the grid. In the future it could also check for invalid spaces
+        inside the grid.
+        """
+        if 0 <= state[0] < self.x_max and 0 <= state[1] < self.y_max:
+            return True
+        return False
+
+    def _generate_world(self):
+        """
+        Creates the gridworld map and places the agent and goal in their corresponding locations.
+        """
+        new_world = [['o' for _ in range(self.x_max)] for _ in range(self.y_max)]
+        new_world[self.curr_state[0]][self.curr_state[1]] = 'x'
+        new_world[self.terminal_state[0]][self.terminal_state[1]] = 'T'
+        return new_world
+
+    def _update_world(self):
+        """
+        Updates the status of the world after a single step
+        """
+        self.world[self.prev_state[0]][self.prev_state[1]] = 'o'
+        self.world[self.curr_state[0]][self.curr_state[1]] = 'x'
 
     def _step(self, action):
         action_str = ACTION_MEANING[action]
+        self.prev_state = self.curr_state.copy()
         if action_str == "UP":
-            self.curr_state[0] -= 1
+            if self._is_valid((self.curr_state[0] - 1, self.curr_state[1])):
+                self.curr_state[0] -= 1
         elif action_str == "DOWN":
-            self.curr_state[0] += 1
+            if self._is_valid((self.curr_state[0] + 1, self.curr_state[1])):
+                self.curr_state[0] += 1
         elif action_str == "LEFT":
-            self.curr_state[1] -= 1
+            if self._is_valid((self.curr_state[0], self.curr_state[1] - 1)):
+                self.curr_state[1] -= 1
         elif action_str == "RIGHT":
-            self.curr_state[1] += 1
-
-        if self.curr_state[0] < 0:
-            self.curr_state[0] = 0
-        elif self.curr_state[0] > self.x_y_dim-1:
-            self.curr_state[0] = self.x_y_dim-1
-        if self.curr_state[1] < 0:
-            self.curr_state[1] = 0
-        elif self.curr_state[1] > self.x_y_dim-1:
-            self.curr_state[1] = self.x_y_dim-1
+            if self._is_valid((self.curr_state[0], self.curr_state[1] + 1)):
+                self.curr_state[1] += 1
+        self._update_world()
 
         observation = self.curr_state
         reward = -1
@@ -52,17 +77,13 @@ class GridWorldEnv(gym.Env):
     def _reset(self):
         self.done = False
         self.curr_state = [0, 0]
-        self.terminal_state = (self.x_y_dim-1, self.x_y_dim-1)
+        self.terminal_state = (self.x_max-1, self.y_max-1)
         return self.curr_state
 
     def _render(self, mode='human', close=False):
         if mode == 'human' or mode == 'ansi':
-            world = [['o' for _ in range(self.x_y_dim)] for _ in range(self.x_y_dim)]
-            world[self.curr_state[0]][self.curr_state[1]] = 'x'
-            world[self.terminal_state[0]][self.terminal_state[1]] = 'T'
-
             outfile = StringIO() if mode == 'ansi' else sys.stdout
-            for row in world:
+            for row in self.world:
                 for cell in row:
                     outfile.write((cell + ' '))
                 outfile.write('\n')
