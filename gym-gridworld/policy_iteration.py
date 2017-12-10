78dc2383cd28d0d838fb6a46182f219f18ac40dd
Unknown <fgtoralesch@gmail.com>
2017-12-10 01:07:41 +0100

    Fix policy iteration, first working version
    First working version of policy iteration on gridworld

--- gym-gridworld/policy_iteration.py
+++ gym-gridworld/policy_iteration.py
@@ -50,19 +50,23 @@ def greedy_policy_from_value_function(policy, env, value_function, discount_fact
     return policy
 
 
-def policy_iteration(policy, env, value_function=None, threshold=0.00001, **kwargs):
+def policy_iteration(policy, env, value_function=None, threshold=0.00001, max_steps=1000, **kwargs):
     """
     Policy iteration algorithm, which consists on iteratively evaluating a policy and updating it greedily with
     respect to the value function obtained from a single step evaluation.
     """
-    delta = 0
     value_function = np.zeros(env.world.size) if value_function is None else value_function
+    step_number = 0
     while True:
-        policy_value = single_step_policy_evaluation(policy, env, value_function, **kwargs)
-        greedy_policy = greedy_policy_from_value_function(policy_value, env, **kwargs)
+        policy_value = single_step_policy_evaluation(policy, env, value_function=value_function, **kwargs)
+        delta = np.max(value_function - policy_value)
+        value_function = policy_value
 
-        if delta < threshold:
+        greedy_policy = greedy_policy_from_value_function(policy, env, value_function=policy_value, **kwargs)
+        step_number += 1
+        if delta < threshold or step_number == max_steps:
             break
+
     return policy_value, greedy_policy
 
 
@@ -73,11 +77,16 @@ if __name__ == '__main__':
     policy0 = np.ones([gw_env.world.size, len(gw_env.actions_list)]) / len(gw_env.actions_list)
     v0 = np.zeros(gw_env.world.size)
     val_fun = v0
-    for k in range(10):
+    for k in range(500):
         val_fun = single_step_policy_evaluation(policy0, gw_env, value_function=val_fun)
     print(reshape_as_gridworld(val_fun))
 
     # test greedy policy
     policy1 = greedy_policy_from_value_function(policy0, gw_env, val_fun)
     policy_map1 = get_policy_map(policy1)
-    print('Policy:\n', policy_map1)
+    print('Policy: (0=up, 1=right, 2=down, 3=left)\n', policy_map1)
+
+    # test policy iteration
+    optimal_value, optimal_policy = policy_iteration(policy0, gw_env, v0, threshold=0.001, max_steps=100)
+    print('Value:\n', reshape_as_gridworld(optimal_value))
+    print('Policy:\n', get_policy_map(optimal_policy))
