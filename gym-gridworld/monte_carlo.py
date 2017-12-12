0ea9f09278693853bde4b7c9e1587d23021fd49f
Unknown <fgtoralesch@gmail.com>
2017-12-12 03:08:23 +0100

    Wrap monte carlo methods
    Added an input to select between the different monte carlo modes for better reusability of the code.

--- gym-gridworld/monte_carlo.py
+++ gym-gridworld/monte_carlo.py
@@ -3,26 +3,29 @@ from gym_gridworld.envs.gridworld_env import GridWorldEnv
 import warnings
 
 
-def monte_carlo_first_visit():
+def run_episode(policy):
+    return np.zeros(1), 0
+
+
+def monte_carlo_first_visit(policy, env, visit_flag='first-visit'):
     """"
-    To evaluate state s
-    The first time-step t that state s is visited in an episode,
-    Increment counter N(s) ← N(s) + 1
-    Increment total return S(s) ← S(s) + Gt
-    Value is estimated by mean return V(s) = S(s)/N(s)
-    By law of large numbers, V(s) → vπ(s) as N(s) → ∞
-    """
-    pass
+    Monte Carlo algorithm, which solves the MDP learning from full episodes without the need of a model.
 
+    It is implemented in three variants depending on how the value function is calculated.
+    """
+    value_function = visit_counter = np.zeros(env.world.size)
+    states_history, total_reward = run_episode(policy)
+    for state in np.nditer(np.arange(value_function.size)):
+        if visit_flag == 'first-visit':
+            visit_counter[state] = 1 if state in states_history else 0
+            value_function += value_function[(visit_counter == 1)]
+        elif visit_flag == 'every-visit':
+            raise NotImplementedError
+        elif visit_flag == 'incremental':
+            raise NotImplementedError
+        else:
+            raise ValueError('Invalid visit flag. Please choose one of the following values: \'first-visit\', '
+                             '\'every-visit\' or \'incremental\'')
+    return value_function
 
-def monte_carlo_every_visit():
-    """
-    To evaluate state s
-    Every time-step t that state s is visited in an episode,
-    Increment counter N(s) ← N(s) + 1
-    Increment total return S(s) ← S(s) + Gt
-    Value is estimated by mean return V(s) = S(s)/N(s)
-    Again, V(s) → vπ(s) as N(s) → ∞
-    """
-    pass
 
