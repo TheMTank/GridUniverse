4e2cf61a79d991decff8744056b2163832426cfd
beduffy <benfduffy@gmail.com>
2017-12-26 19:42:09 +0000

    Added 2nd unit test: test_default_gridworld_completion_in_six_steps()

--- tests/tests.py
+++ tests/tests.py
@@ -9,12 +9,26 @@ class TestGridWorld(unittest.TestCase):
         observation = env.reset()
         env.render()
         action = 1 # go right
-        print('go ' + env.action_descriptors[action])
 
         observation, reward, done, info = env.step(action)
 
         env.render()
         self.assertTrue(observation == 0)
+
+    def test_default_gridworld_completion_in_six_steps(self):
+        env = GridWorldEnv()
+
+        actions_to_take = [1, 1, 1, 0, 0, 0] # 3 rights and 3 ups
+        observation = env.reset()
+        for t in range(100):
+            env.render()
+            action = actions_to_take[t]
+            observation, reward, done, info = env.step(action)
+
+            if done:
+                print("Episode finished after {} timesteps".format(t + 1))
+                self.assertTrue((t + 1) == 6)
+                break
 
 if __name__ == '__main__':
     unittest.main()
