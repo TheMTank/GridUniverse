57c6da5d2f67f143da2f3783561e98a06e26f35d
beduffy <benfduffy@gmail.com>
2017-12-28 19:32:53 +0000

    Finished Monte-Carlo 95%.
    1. Comments describing the algorithm
    2. New methodology of not storing all returns, rewards, states but using last episode's info wisely
    3. Done model-free
    4. Non-stationary env can now take alpha parameter
    5. Non-incremental mean works.
    6. Much clearer

--- gym-gridworld/monte_carlo.py
+++ gym-gridworld/monte_carlo.py
@@ -25,117 +25,86 @@ def run_episode(policy, env, max_steps_per_episode=100):
 
 
 def monte_carlo_evaluation(policy, env, every_visit=False, incremental_mean=True, stationary_env=True,
-                           discount_factor=0.01, threshold=0.0001, n_episodes=100):
+                           discount_factor=0.01, threshold=0.0001, alpha=0.001, n_episodes=100):
     """"
     Monte Carlo algorithm, which solves the MDP learning from full episodes without the need of a model.
 
     It is implemented in three variants depending on how the value function is calculated.
+    # Monte-Carlo
+    1. Store visit counts for each state from last episode (First Visit MC vs Every Visit MC count differently)
+    2. If incremental mean, update total_visit_counter and using that and returns from last episode update
+       value function at the end of each episode.
+    3. If not using incremental mean store total return for each state over all episodes
+       (again depending on First Visit MC vs Every Visit MC) and using this, update value function after all episodes have been run
+    4. If non-stationary environment use alpha instead of (1 / (total_visit_counter[state]))
     """
-    # visit_counter = total_return = np.zeros(env.world.size) # do it model-free
-    visit_counter = {}
+
+    total_visit_counter = {} # could do "total_visit_counter = np.zeros(env.world.size)" but that isn't model-free
     total_return = {}
     value_function = {}
-    # episodes_states_history = np.empty([1], dtype=np.int)
-    # episodes_reward_history = np.empty([1], dtype=np.int32)
-    all_episode_state_hist = []
-    all_episode_reward_hist = []
     for episode in range(n_episodes):
         episode_state_hist, episode_reward_hist = run_episode(policy, env)
-        # np.append(all_episode_state_hist, episode_state_hist)
-        # np.append(all_episode_reward_hist, episode_reward_hist)
-        # all_episode_state_hist.extend(episode_state_hist)
-        # all_episode_reward_hist.extend(episode_reward_hist)
 
-        # todo after each episode: every visit
-        # todo 1. store visit counts for each state
-        # todo 2. store total return for each visit
-        # todo 3. Update V(St)
-
-        # Store visit counts for each state from last episode
-        for state in episode_state_hist:
-            if state not in visit_counter:
-                visit_counter[state] = 1
-                total_return[state] = 0
-            else:
-                visit_counter[state] += 1
-
+        visits_from_last_episode = {}
         returns_from_last_episode = {}
-        # Store returns from each state from last episode
+        # Store visit counts and returns from each state from last episode depending on whether first or every visit MC
         for idx, (state, reward) in enumerate(zip(episode_state_hist, episode_reward_hist)):
-            # return_from_episode_from_state = sum([(discount_factor ** i) * r for i, r in enumerate(episode_reward_hist[idx:])
-            #                                     if (discount_factor ** i) < threshold])
-            # total_return[state] += return_from_episode_from_state
-            return_from_episode_from_state = sum([(discount_factor ** i) * r for i, r in enumerate(episode_reward_hist[idx:])
-                                                if (discount_factor ** i) < threshold])
-            if state not in returns_from_last_episode:
-                return_from_episode_from_state[state] = 0.0
+            if state not in visits_from_last_episode: # todo put all below, DRY
+                visits_from_last_episode[state] = 0
+                returns_from_last_episode[state] = 0.0
             else:
                 if not every_visit:
-                    continue # State was already seen before in episode, don't add more return to that state
+                    # State was already seen before in episode, don't add more return to that state if First Visit MC
+                    # Also don't increase it's visit count
+                    continue
 
-            returns_from_last_episode[state] += return_from_episode_from_state # Add if statement if first visit
+            # Calculation depends heavily if whether it is first or every visit MC.
+            # Update visit counter for last episode
+            visits_from_last_episode[state] += 1
+            # Update return for last episode for a specific state
+            return_from_state = sum([(discount_factor ** i) * r for i, r in enumerate(episode_reward_hist[idx:])
+                                                  if (discount_factor ** i) < threshold]) # todo mention bug in commit
+            returns_from_last_episode[state] += return_from_state # Add if statement if first visit
+            # todo check that return is just summed up together and then only at end of episode do you do error term update (not for each update)
+
+        # Update total_visit_counter, total_return, and value function for each state
+        for state in visits_from_last_episode:
+            # if state hasn't been seen before, setup defaults
+            if state not in total_visit_counter:
+                total_visit_counter[state] = 0
+                total_return[state] = 0.0
+                value_function[state] = 0.0
+
+            # Update visits from last episode
+            total_visit_counter[state] += visits_from_last_episode[state]
+            # Update total return. Only used for non-incremental mean.
+            if not incremental_mean:
+                """
+                Increment total return S(s) ← S(s) + Gt
+                """
+                total_return[state] += returns_from_last_episode[state]
+            else:
+                # Update value function only if incremental mean. Otherwise do at end
+                if stationary_env:
+                    """
+                    V(St) ← V(St) + 1/ N(St) * (Gt − V(St))
+                    """
+                    value_function[state] += (1 / total_visit_counter[state]) * (returns_from_last_episode[state] - value_function[state])
+                else:
+                    """
+                    In non-stationary problems, it can be useful to track a running mean, i.e. forget old episodes.
+                    V(St) ← V(St) + α(Gt − V(St))
+                    """
+                    value_function[state] += alpha * (returns_from_last_episode[state] - value_function[state])
 
 
-            # for i in range(episodes_states.size - idx):
-            #     # break the loop if the next state contribution is below the threshold
-            #     if (discount_factor ** i) > threshold: # bug should be less than?
-            #         total_return[state] += (discount_factor ** i) * episodes_reward[i]
-            #     else:
-            #         break
-
-
-
-        # if over: V(s) = S(s) / N(s)
-
-
-    #     # count visits
-    #     if not every_visit:
-    #         raise NotImplementedError
-    #         # TODO: this part is wrong
-    #         # episodes_states = set(episodes_states)
-    #         # unique_episodes_states, indeces = np.unique(episodes_states, return_inverse=True)
-    #
-    #     visit_counter += np.fromiter((1 if state in episodes_states else 0
-    #                                   for state in np.nditer(np.arange(value_function.size))), dtype=np.uint16)
-    #
-    #     if incremental_mean:
-    #         # add observation values dynamically
-    #         """
-    #         V(St) ← V(St) + 1/ N(St) * (Gt − V(St))
-    #
-    #         Currently only working for every_visit MC
-    #         """
-    #         for idx, state in enumerate(episodes_states):
-    #             for i in range(episodes_states.size - idx):
-    #                 # break the loop if the next state contribution is below the threshold
-    #                 if (discount_factor ** i) > threshold:
-    #                     total_return[state] += (discount_factor ** i) * episodes_reward[i]
-    #                 else:
-    #                     break
-    #             value_function[state] += (total_return[state] - value_function[state]) / visit_counter[state]
-    #     else:
-    #         """
-    #         Increment total return S(s) ← S(s) + Gt
-    #         """
-    #         raise NotImplementedError
-    #         #  TODO: wrong, we need bellman eq
-    #         # add observations to the total return for a static mean
-    #         # total_return[(visit_counter > 0)] += reward_history[(visit_counter > 0)]
-    #
-    #     if not stationary_env:
-    #         """
-    #         In non - stationary problems, it can be useful to track a running mean, i.e.forget old episodes.
-    #         V(St) ← V(St) + α(Gt − V(St))
-    #         """
-    #         raise NotImplementedError
-    #
-    # if not incremental_mean:
-    #     """
-    #     Value is estimated by mean return V(s) = S(s) / N(s)
-    #     """
-    #     raise NotImplementedError
-    #     # total_return[(visit_counter > 0)] += reward_history[(visit_counter > 0)]  # TODO: wrong, we need bellman eq
-    #     # value_function += total_return/visit_counter
+    if not incremental_mean:
+        """
+        Value is estimated by mean return V(s) = S(s) / N(s) if not incremental mean
+        """
+        for state in total_visit_counter:
+            value_function[state] = total_return[state] / total_visit_counter[state]
+        # TODO: wrong, we need bellman eq ?
 
     return value_function
 
@@ -146,5 +115,7 @@ if __name__ == '__main__':
     st_history, rw_history = run_episode(policy0, env)
     print('States history: ' + str(st_history))
     print('Rewards history: ' + str(rw_history))
-    # value0 = monte_carlo_evaluation(policy0, gw_env, True)
-    # print(value0)
+    value0 = monte_carlo_evaluation(policy0, env, True)
+    print(value0)
+    for state, value in value0.items():
+        print(state, value)
