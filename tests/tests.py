821f207f327215fd0e1fd4ed89d805611a371fed
beduffy <benfduffy@gmail.com>
2018-01-02 22:54:08 +0000

    Tried to get pytests working
    unittests stopped working with PyCharm and in general import isn't working (2 different problems)

--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,6 @@ from core.envs.gridworld_env import GridWorldEnv
 class TestGridWorld(unittest.TestCase):
     def test_gridworld_wall(self):
         env = GridWorldEnv(walls=[4])
-        observation = env.reset()
         env.render()
         action = 1 # go right
 
@@ -19,7 +18,6 @@ class TestGridWorld(unittest.TestCase):
         env = GridWorldEnv()
 
         actions_to_take = [1, 1, 1, 0, 0, 0] # 3 rights and 3 ups
-        observation = env.reset()
         for t in range(100):
             env.render()
             action = actions_to_take[t]
