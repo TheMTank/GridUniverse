c3e93ca846c5b97c8a23e190732556e1dad1d9f9
beduffy <benfduffy@gmail.com>
2018-01-05 21:01:31 +0000

    Renamed test.py to test_gridworld.py
    PyCharm unittests wasn't working otherwise.

--- tests/test_gridworld.py
+++ tests/test_gridworld.py
@@ -6,20 +6,18 @@ from core.envs.gridworld_env import GridWorldEnv
 class TestGridWorld(unittest.TestCase):
     def test_gridworld_wall(self):
         env = GridWorldEnv(walls=[4])
-        observation = env.reset()
         env.render()
         action = 1 # go right
 
         observation, reward, done, info = env.step(action)
 
         env.render()
-        self.assertTrue(observation == 0)
+        self.assertTrue(observation == 0) # check if in same place
 
     def test_default_gridworld_completion_in_six_steps(self):
         env = GridWorldEnv()
 
         actions_to_take = [1, 1, 1, 0, 0, 0] # 3 rights and 3 ups
-        observation = env.reset()
         for t in range(100):
             env.render()
             action = actions_to_take[t]
